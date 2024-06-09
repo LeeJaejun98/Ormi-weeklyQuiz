25af5f4dd103563df856f74b59e8e16b9d19ee86
LeeJaejun98 <jj980302@naver.com>
2024-06-09 20:38:44 +0900

    error catch

--- src/Quiz1.java
+++ src/Quiz1.java
@@ -9,32 +9,46 @@ public class Quiz1 {
             System.out.println("원하는 기능을 선택하세요");
             System.out.println("1. 덧셈 /2. 뺄셈 /3. 곱셈 /4. 나눗셈 /0. 종료");
             operator = sc.nextInt();
+
             if(operator == 0){
                 System.out.println("시스템을 종료합니다.");
                 break;
-            }
-            System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
-            a = sc.nextInt();
-            System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
-            b = sc.nextInt();
-
-            switch (operator) {
-                case 1:
-                    System.out.println(a + " + " + b + " = " + (a + b));
-                    break;
-                case 2:
-                    System.out.println(a + " - " + b + " = " + (a - b));
-                    break;
-                case 3:
-                    System.out.println(a + " * " + b + " = " + (a * b));
-                    break;
-                case 4:
-                    System.out.println(a + " / " + b + " = " + ((double)a / b));
-                    break;
-                default:
-                    System.out.println("올바른 입력이 아닙니다.");
-                    break;
+            }else {
+                switch (operator) {
+                    case 1:
+                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
+                        a = sc.nextInt();
+                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
+                        b = sc.nextInt();
+                        System.out.println(a + " + " + b + " = " + (a + b));
+                        break;
+                    case 2:
+                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
+                        a = sc.nextInt();
+                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
+                        b = sc.nextInt();
+                        System.out.println(a + " - " + b + " = " + (a - b));
+                        break;
+                    case 3:
+                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
+                        a = sc.nextInt();
+                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
+                        b = sc.nextInt();
+                        System.out.println(a + " * " + b + " = " + (a * b));
+                        break;
+                    case 4:
+                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
+                        a = sc.nextInt();
+                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
+                        b = sc.nextInt();
+                        System.out.println(a + " / " + b + " = " + ((double)a / b));
+                        break;
+                    default:
+                        System.out.println("올바른 입력이 아닙니다.");
+                        break;
+                }
             }
         }
     }
 }
+
