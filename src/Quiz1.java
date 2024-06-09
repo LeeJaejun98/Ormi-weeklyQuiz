d6c9a430937a9fec2c04a29157190305e73712e9
LeeJaejun98 <jj980302@naver.com>
2024-06-09 21:02:15 +0900

    refactoring switch case

--- src/Quiz1.java
+++ src/Quiz1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Quiz1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,45 +11,44 @@ public class Quiz1 {
             System.out.println("1. 덧셈 /2. 뺄셈 /3. 곱셈 /4. 나눗셈 /0. 종료");
             operator = sc.nextInt();
 
-            if(operator == 0){
+            if (operator == 0) {
                 System.out.println("시스템을 종료합니다.");
                 break;
-            }else {
-                switch (operator) {
-                    case 1:
-                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
-                        a = sc.nextInt();
-                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
-                        b = sc.nextInt();
-                        System.out.println(a + " + " + b + " = " + (a + b));
-                        break;
-                    case 2:
-                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
-                        a = sc.nextInt();
-                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
-                        b = sc.nextInt();
-                        System.out.println(a + " - " + b + " = " + (a - b));
-                        break;
-                    case 3:
-                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
-                        a = sc.nextInt();
-                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
-                        b = sc.nextInt();
-                        System.out.println(a + " * " + b + " = " + (a * b));
-                        break;
-                    case 4:
-                        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
-                        a = sc.nextInt();
-                        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
-                        b = sc.nextInt();
-                        System.out.println(a + " / " + b + " = " + ((double)a / b));
-                        break;
-                    default:
-                        System.out.println("올바른 입력이 아닙니다.");
-                        break;
-                }
+            }
+            switch (operator) {
+                case 1:
+                    inputNum();
+                    System.out.println(a + " + " + b + " = " + (a + b));
+                    break;
+                case 2:
+                    inputNum();
+                    System.out.println(a + " - " + b + " = " + (a - b));
+                    break;
+                case 3:
+                    inputNum();
+                    System.out.println(a + " * " + b + " = " + (a * b));
+                    break;
+                case 4:
+                    inputNum();
+                    System.out.println(a + " / " + b + " = " + ((double) a / b));
+                    break;
+                default:
+                    System.out.println("올바른 입력이 아닙니다.");
+                    break;
             }
         }
+    }
+    static int[] inputNum(){
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[2];
+
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요.");
+        nums[0] = sc.nextInt();
+
+        System.out.println("두번째 값을 입력하고 엔터를 누르세요.");
+        nums[1] = sc.nextInt();
+
+        return nums;
     }
 }
 
