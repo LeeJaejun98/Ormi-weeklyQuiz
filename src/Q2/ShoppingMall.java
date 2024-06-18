661363691358966a387fcc69a098b33e1a2050c7
LeeJaejun98 <jj980302@naver.com>
2024-06-18 23:10:23 +0900

    removeProduct

--- src/Q2/ShoppingMall.java
+++ src/Q2/ShoppingMall.java
@@ -1,5 +1,7 @@
 package Q2;
 
+import java.util.Arrays;
+
 public abstract class ShoppingMall {
     private Product[] productArray;
     private int arraySize;
@@ -23,12 +25,28 @@ public abstract class ShoppingMall {
         productArray[cnt++] = product;
     }
 
-    public void removeProduct(String productName) {
+    public void removeProduct(Product product) {
+        int index = findIndex(product);
+        if (index != -1) {
+            if(index >= 0 && index < cnt){
+               for (int i = index; i < productArray.length - 1 ; i++){
+                   productArray[i] = productArray[i+1];
+               }
+               productArray = Arrays.copyOf(productArray, productArray.length - 1);
+            }
+        }
+    }
 
+    public int findIndex(Product product) { // productArray에서 매치되는 index 찾기
+        for (int i = 0; i < cnt; i++) {
+            if (productArray[i].equals(product))
+                return i;
+        }
+        return -1;
     }
 
     public void displayProducts() {
-        for (int i = 0; i < cnt; i++){
+        for (int i = 0; i < cnt; i++) {
             System.out.println("product name : " + productArray[i].getProductName());
             System.out.println("product price : " + productArray[i].getPrice());
             System.out.println("product stock : " + productArray[i].getInventory());
