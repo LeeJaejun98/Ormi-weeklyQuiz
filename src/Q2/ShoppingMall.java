cc5e2fda19286d1dedcb263e1b29253b3bec3919
LeeJaejun98 <jj980302@naver.com>
2024-06-18 22:30:31 +0900

    construct addProduct, displayProducts

--- src/Q2/ShoppingMall.java
+++ src/Q2/ShoppingMall.java
@@ -1,27 +1,38 @@
 package Q2;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+public abstract class ShoppingMall {
+    private Product[] productArray;
+    private int arraySize;
+    private int cnt;
 
-public class ShoppingMall {
-    ArrayList arrayList;
-
-    abstract void checkOrderAvailablilty(){
-
-    }
-    public ShoppingMall(ArrayList arrayList) {
-        this.arrayList = arrayList;
+    public ShoppingMall(Product[] productArray, int arraySize) {
+        this.productArray = new Product[arraySize];
+        cnt = 0;
     }
 
-    public void addProduct(){
+    abstract boolean checkOrderAvailability(Product product);
+
+    public void addProduct(Product product) { // count 필요함
+        if (cnt == productArray.length) {
+            Product[] addedArray = new Product[productArray.length * 2];
+            for (int i = 0; i < productArray.length; i++) {
+                addedArray[i] = productArray[i];
+            }
+            productArray = addedArray;
+        }
+        productArray[cnt++] = product;
+    }
+
+    public void removeProduct(String productName) {
 
     }
 
-    public void deleteProduct(){
-
+    public void displayProducts() {
+        for (int i = 0; i < cnt; i++){
+            System.out.println("product name : " + productArray[i].getProductName());
+            System.out.println("product price : " + productArray[i].getPrice());
+            System.out.println("product stock : " + productArray[i].getInventory());
+        }
     }
 
-    public void printProductList(){
-
-    }
 }
