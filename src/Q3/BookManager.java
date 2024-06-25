cb34749fe605fa412d1bf85948be71f41945af6b
LeeJaejun98 <jj980302@naver.com>
2024-06-25 23:07:06 +0900

    bookManager Detail fix

--- src/Q3/BookManager.java
+++ src/Q3/BookManager.java
@@ -3,8 +3,8 @@ package Q3;
 import java.util.List;
 
 public interface BookManager<T> {
-    abstract void addBook(Book<T> book);
-    abstract void removeBook(Book<T> book);
-    abstract List<Book<T>> searchByTitle(String title);
-    List<Book<T>> searchByAuthor(String author);
+    void addBook(Book<T> book);    //책 추가 메소드
+    void removeBook(Book<T> book); // 책 삭제 메소드
+    List<Book<T>> searchByTitle(String title);   //책 제목으로 검색
+    List<Book<T>> searchByAuthor(String author); // 책 저자로 검색
 }
