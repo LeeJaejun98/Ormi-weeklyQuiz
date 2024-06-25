f68b4a5d38f98d58a285639e6c2bb10dd8f212c2
LeeJaejun98 <jj980302@naver.com>
2024-06-25 23:07:33 +0900

    BookStack Detail fix

--- src/Q3/BookStack.java
+++ src/Q3/BookStack.java
@@ -1,8 +1,37 @@
 package Q3;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class BookStack<T> {
     private Stack<Book<T>> books;   //Book 객체 저장하는 Stack
 
+    public BookStack(){
+        books = new Stack<>();
+    }
+
+    public void pushBook(Book<T> book){
+        books.push(book);
+    }
+
+    public Book<T> popBook(){
+        if(books.empty()){
+            throw new EmptyStackException();
+        }
+        return books.pop();
+    }
+
+    public Book<T> peekBook(){
+        if(books.empty()){
+            throw new EmptyStackException();
+        }
+        return books.peek();
+    }
+
+    public boolean isEmpty(){
+        if (books.empty()){
+            return true;
+        }
+        return false;
+    }
 }
