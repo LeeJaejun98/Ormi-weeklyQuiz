e5c1de83285bac291333948cd0c1d65da067e5fc
LeeJaejun98 <jj980302@naver.com>
2024-06-25 23:07:19 +0900

    BookShelf Detail fix

--- src/Q3/BookShelf.java
+++ src/Q3/BookShelf.java
@@ -1,33 +1,60 @@
 package Q3;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class BookShelf implements BookManager {
+public class BookShelf<T> implements BookManager<T> {
 
     private List<Book<T>> books;    // Book 객체를 저장하는 ArrayList
+    private static final int EQUALS = 0;
 
-    public BookShelf(){
-
+    public BookShelf() {
+        this.books = new ArrayList<>();
     }
+
     @Override
-    public void addBook(Book book) {
+    public void addBook(Book<T> book) {
         books.add(book);
     }
 
     @Override
-    public void removeBook(Book book) {
+    public void removeBook(Book<T> book) {
         if (!books.isEmpty()){
             books.remove(book);
         }
     }
 
     @Override
-    public List<Book> searchByTitle(String title) {
-        return null;
+    public List<Book<T>> searchByTitle(String title) {
+        List<Book<T>> res = new ArrayList<>();
+        for(Book<T> book: books){
+            if(isTitleEqualsOrContains(book, title)){
+                res.add(book);
+            }
+        }
+        return res;
     }
 
     @Override
-    public List<Book> searchByAuthor(String author) {
-        return null;
+    public List<Book<T>> searchByAuthor(String author) {
+        List<Book<T>> res = new ArrayList<>();
+        for(Book<T> book : books){
+            if(isAuthorEqualsOrContains(book, author)){
+                res.add(book);
+            }
+        }
+        return res;
+    }
+
+    private boolean isTitleEqualsOrContains(Book<T> book, String title){
+        boolean equal = title.compareToIgnoreCase(book.getTitle()) == EQUALS;
+        boolean contains = title.contains(book.getTitle());
+        return equal || contains;
+    }
+
+    private boolean isAuthorEqualsOrContains(Book<T> book, String author){
+        boolean equal = author.compareToIgnoreCase(book.getAuthor()) == EQUALS;
+        boolean contains = author.contains(book.getAuthor());
+        return equal || contains;
     }
 }
